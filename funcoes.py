4d6dc58782972f8ba9dcd626944db60154284607
jvictuur <joaov04.2024@gmail.com>
2024-10-26 14:59:16 -0300

    Questão 2: Preenche Frota

--- funcoes.py
+++ funcoes.py
@@ -9,3 +9,13 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
             posicoes.append([linha, coluna + i])
     
     return posicoes
+
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    posicoes_nav = define_posicoes(linha, coluna, orientacao, tamanho)
+    
+    if nome_navio in frota:
+        frota[nome_navio].append(posicoes_nav)
+    else:
+        frota[nome_navio] = [posicoes_nav]
+    
+    return frota
