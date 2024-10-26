937f3dd105cf97e1f877c2f5e0d60cb0cf97313b
jvictuur <joaov04.2024@gmail.com>
2024-10-26 15:08:44 -0300

    Questão 3: Faz Jogada

--- funcoes.py
+++ funcoes.py
@@ -19,3 +19,11 @@ def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
         frota[nome_navio] = [posicoes_nav]
     
     return frota
+
+def faz_jogada(tabuleiro, linha, coluna):
+    if tabuleiro[linha][coluna] == 1:
+        tabuleiro[linha][coluna] = 'X' 
+    else:
+        tabuleiro[linha][coluna] = '-' 
+
+    return tabuleiro
